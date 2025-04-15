6857ef110d9e9a3d465ff872a4f1debe32771d1b
Krystian Wojda <krystianwojda@gmail.com>
2025-04-16 01:29:27 +0200

    nozbe rss app

--- src/contexts/AppContext.ts
+++ src/contexts/AppContext.ts
@@ -11,22 +11,22 @@ export interface AppSettings {
     theme: string
 }
 
-export interface AppNews {
-    guid: string
-    title: string
-    description: string
-    link: string
-    pubDate: string
-    image: string
-}
+// export interface AppNews {
+//     guid: string
+//     title: string
+//     description: string
+//     link: string
+//     pubDate: string
+//     image: string
+// }
 
-export interface AppFeed {
-    id: string
-    title: string
-    description: string
-    link: string
-    news: AppNews[]
-}
+// export interface AppFeed {
+//     id: string
+//     title: string
+//     description: string
+//     link: string
+//     news: AppNews[]
+// }
 
 export type AppData = Record<string, unknown>
 
@@ -34,8 +34,6 @@ export interface AppContextProps {
     settings: AppSettings
     user: AppUser
     data: AppData[]
-    feeds: AppFeed[]
-    save: (fieldName: string, newData: AppData) => void
 }
 
 export const AppContext = createContext<AppContextProps | null>(null)
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,1 @@
 export * from "./useAppContext"
-export * from "./useFetchRSS"
-export * from "./useFetchFeed"
